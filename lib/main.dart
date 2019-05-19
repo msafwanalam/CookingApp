768277fac40aa10e601e29bcb4198cf50eae4b68
Cannedtuna121 <thecan2013@gmail.com>
2019-05-19 19:05:43 -0400

    Added some basic ability to asses the camera. Still needs to actully do everything this is just a basic setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
 
-void main() => runApp(MyApp());
+import 'dart:io';
+
+
+Future<void>main() async{
+final List<CameraDescription> cameras = await availableCameras();
+final CameraDescription firstCamera = cameras.first;
+
+runApp(
+  MaterialApp(
+    theme: ThemeData.dark(),
+    home: TakePictureScreen(camera: firstCamera)
+  )
+
+);
+
+}
+
+class TakePictureScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  const TakePictureScreen({
+    Key key,
+    @required this.camera,
+  }) : super(key: key);
+
+  @override
+  TakePictureScreenState createState() => TakePictureScreenState();
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  // Add two variables to the state class to store the CameraController and
+  // the Future
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    // In order to display the current output from the Camera, you need to
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras
+      widget.camera,
+      // Define the resolution to use
+      ResolutionPreset.high,
+    );
+
+    // Next, you need to initialize the controller. This returns a Future
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Make sure to dispose of the controller when the Widget is disposed
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Take a picture')),
+      // You must wait until the controller is initialized before displaying the
+      // camera preview. Use a FutureBuilder to display a loading spinner until
+      // the controller has finished initializing
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        // Provide an onPressed callback
+        onPressed: () async {
+          // Take the Picture in a try / catch block. If anything goes wrong,
+          // catch the error.
+          try {
+            // Ensure the camera is initialized
+            await _initializeControllerFuture;
+
+            // Construct the path where the image should be saved using the path
+            // package.
+            final path = join(
+              // In this example, store the picture in the temp directory. Find
+              // the temp directory using the `path_provider` plugin.
+              (await getApplicationDocumentsDirectory()).path, '${DateTime.now()}.png',
+            );
+            print("\n\n"+path+"\n");
+            // Attempt to take a picture and log where it's been saved
+            await _controller.takePicture(path);
+
+            // If the picture was taken, display it on a new screen
+            Image.file(File('picture.png'));
+          } catch (e) {
+            // If an error occurs, log the error to the console.
+            print(e);
+          }
+        },
+      ),
+    );
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -42,6 +154,9 @@ class MyHomePage extends StatefulWidget {
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
+
+
+
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -55,6 +170,8 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+
+
     });
   }
 
